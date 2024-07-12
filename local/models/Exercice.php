2f531dd185897267ccb7425a67b9c0b2ea09bf82
harishammache <hammache_haris@hotmail.fr>
2024-07-12 19:13:42 +0200

    Addition of the getCommentsByPostId function that retrieves comments for an article from the posts_comments and users tables

--- local/models/Exercice.php
+++ local/models/Exercice.php
@@ -34,4 +34,19 @@ class Exercice extends CommonModels
         $result->execute();
         return $result->fetch(PDO::FETCH_ASSOC);
     }
+
+    public function getCommentsByPostId($postId) {
+        $pdo    = $this->pdoConnect();
+        $result = $pdo->prepare("SELECT 
+                                    users.firstname,
+                                    posts_comments.comment_date,
+                                    posts_comments.comment_text
+                                FROM posts_comments
+                                INNER JOIN users ON posts_comments.user_id = users.id_user
+                                WHERE posts_comments.post_id = :postId
+                                ORDER BY posts_comments.comment_date DESC");
+        $this->bind($result, ':postId', $postId);
+        $result->execute();
+        return $result->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
