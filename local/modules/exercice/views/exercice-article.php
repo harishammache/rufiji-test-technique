5e9d50162264f351f8a80da389a5749a82b5994d
harishammache <hammache_haris@hotmail.fr>
2024-07-12 19:18:59 +0200

    Added loop to display comments with author's first name, formatted date, and comment text, and refactored the code for CSS styling purposes

--- local/modules/exercice/views/exercice-article.php
+++ local/modules/exercice/views/exercice-article.php
@@ -1,13 +1,32 @@
-<link rel="stylesheet" href="<?= WEBSITE_URL ?>/public/css/exercice.css">
-<div class="body-content">
-    <div class="container">
-        Titre de l'article : <?= $this->view['posts']['title'] ?>
-        <br>
-        Texte de l'article : <?= $this->view['posts']['content'] ?>
-        <br>
-        Auteur de l'article : <?= $this->view['posts']['author']['firstname'] . ' ' . $this->view['posts']['author']['lastname'] ?>
-
-        <br/><br/>
-        <h3>Ajouter les commentaires ci-dessous :</h3>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Article et Commentaires</title>
+    <link rel="stylesheet" href="<?= WEBSITE_URL ?>/public/css/exercice-article.css">
+</head>
+<body>
+    <div class="body-content">
+        <div class="container">
+            <h1><?= $this->view['posts']['title'] ?></h1>
+            
+            <div class="content">
+                <?= $this->view['posts']['content'] ?>
+            </div>
+            <p class="author">Par <?= $this->view['posts']['author']['firstname'] . ' ' . $this->view['posts']['author']['lastname'] ?></p>
+            
+            <div class="comments-section">
+                <h3>Commentaires :</h3>
+                <?php foreach ($this->view['posts']['comments'] as $comment): ?>
+                    <div class="comment">
+                        <p><strong><?= $comment['firstname'] ?></strong> (<?= date('d/m/Y', strtotime($comment['comment_date'])) ?>)</p>
+                        <p><?= $comment['comment_text'] ?></p>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
     </div>
-</div>
+</body>
+</html>
+
